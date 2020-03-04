e44a1fee2393640ea18bda754206107fa6cbcce6
xiepanpan <xiepanpan@thunisoft.com>
2020-03-04 11:12:44 +0800

    :beers: 登录功能

--- gmall-ums/src/main/java/com/xiepanpan/gmall/ums/service/impl/MemberServiceImpl.java
+++ gmall-ums/src/main/java/com/xiepanpan/gmall/ums/service/impl/MemberServiceImpl.java
@@ -1,10 +1,14 @@
 package com.xiepanpan.gmall.ums.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xiepanpan.gmall.ums.entity.Member;
 import com.xiepanpan.gmall.ums.mapper.MemberMapper;
 import com.xiepanpan.gmall.ums.service.MemberService;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 
 /**
  * <p>
@@ -15,6 +19,16 @@ import org.springframework.stereotype.Service;
  * @since 2019-12-06
  */
 @Service
+@Component
 public class MemberServiceImpl extends ServiceImpl<MemberMapper, Member> implements MemberService {
 
+    @Autowired
+    MemberMapper memberMapper;
+
+    @Override
+    public Member login(String username, String password) {
+        String digest = DigestUtils.md5DigestAsHex(password.getBytes());
+        Member member = memberMapper.selectOne(new QueryWrapper<Member>().eq("username", username).eq("password", digest));
+        return member;
+    }
 }
