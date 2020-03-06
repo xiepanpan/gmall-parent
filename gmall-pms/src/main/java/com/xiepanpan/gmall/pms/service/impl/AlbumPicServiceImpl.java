53ccc13c8055f273cee9aeaf49ccc69401ebb347
xiepanpan <xiepanpan@thunisoft.com>
2020-03-06 13:36:38 +0800

    :beers: dubbo注册

--- gmall-pms/src/main/java/com/xiepanpan/gmall/pms/service/impl/AlbumPicServiceImpl.java
+++ gmall-pms/src/main/java/com/xiepanpan/gmall/pms/service/impl/AlbumPicServiceImpl.java
@@ -1,10 +1,11 @@
 package com.xiepanpan.gmall.pms.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.xiepanpan.gmall.pms.entity.AlbumPic;
 import com.xiepanpan.gmall.pms.mapper.AlbumPicMapper;
 import com.xiepanpan.gmall.pms.service.AlbumPicService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 /**
  * <p>
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
  * @since 2019-12-06
  */
 @Service
+@Component
 public class AlbumPicServiceImpl extends ServiceImpl<AlbumPicMapper, AlbumPic> implements AlbumPicService {
 
 }
